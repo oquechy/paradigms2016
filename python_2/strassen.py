4173cd2f5145bcda21edae6499af15ead828697a
oquechy <vasilliza@gmail.com>
2016-09-25 07:08:06 +0300

    loadtxt used

--- python_2/strassen.py
+++ python_2/strassen.py
@@ -1,19 +1,21 @@
 import numpy as np
 from math import log2, ceil
+import sys
 #from random import randint
 
 def strassen(a, b, n):
     if n == 1:
         return a.dot(b)
-    a11, a12, a21, a22 = a[:n // 2, :n // 2], a[:n // 2, n // 2:], a[n // 2:, :n // 2], a[n // 2:, n // 2:]
-    b11, b12, b21, b22 = b[:n // 2, :n // 2], b[:n // 2, n // 2:], b[n // 2:, :n // 2], b[n // 2:, n // 2:]
-    p1 = strassen(a11 + a22, b11 + b22, n // 2)
-    p2 = strassen(a21 + a22, b11, n // 2)
-    p3 = strassen(a11, b12 - b22, n // 2)
-    p4 = strassen(a22, b21 - b11, n // 2)
-    p5 = strassen(a11 + a12, b22, n // 2)
-    p6 = strassen(a21 - a11, b11 + b12, n // 2)
-    p7 = strassen(a12 - a22, b21 + b22, n // 2)
+    m = n // 2
+    a11, a12, a21, a22 = a[:m, :m], a[:m, m:], a[m:, :m], a[m:, m:]
+    b11, b12, b21, b22 = b[:m, :m], b[:m, m:], b[m:, :m], b[m:, m:]
+    p1 = strassen(a11 + a22, b11 + b22, m)
+    p2 = strassen(a21 + a22, b11, m)
+    p3 = strassen(a11, b12 - b22, m)
+    p4 = strassen(a22, b21 - b11, m)
+    p5 = strassen(a11 + a12, b22, m)
+    p6 = strassen(a21 - a11, b11 + b12, m)
+    p7 = strassen(a12 - a22, b21 + b22, m)
     c = np.vstack((np.hstack((p1 + p4 - p5 + p7, p3 + p5)), np.hstack((p2 + p4,p1 + p3 - p2 + p6))))
     return c
 
@@ -38,15 +40,15 @@ def test_strassen(n):
 
 def main():
     m = int(input())
-    a1 = np.array([list(map(int, input().split())) for _ in range(m)])
-    b1 = np.array([list(map(int, input().split())) for _ in range(m)])
+    ab = np.loadtxt(sys.stdin, dtype=np.int, ndmin=2)
     n = 2 ** ceil(log2(m))
     a = np.zeros((n, n), dtype='int')
     b = np.zeros((n, n), dtype='int')
-    a[:m, :m] = a1
-    b[:m, :m] = b1
+    a[:m, :m] = ab[:m, :]
+    b[:m, :m] = ab[m:, :]
     for row in strassen(a, b, n)[:m, :m]:
         print(*row)
+
 
 if __name__ == '__main__':
     main()
